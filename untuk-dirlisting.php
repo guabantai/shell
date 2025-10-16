ed9d663b6f44df857ccbbe62dcd6861aa3e3f3b7
guabantai <wijayahans07@gmail.com>
2025-10-17 04:48:32 +0700

    Create untuk-dirlisting.php

---
+++ untuk-dirlisting.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>FORBIDEN NOT ALLOWED</title>
+</head>
+
+<body bgcolor="#000000">
+<table width="100%" border="0" cellspacing="0" cellpadding="10">
+  <tr>
+    <td>&nbsp;</td>
+    <td align="right" valign="middle"><img src="https://cdn.pixabay.com/photo/2012/04/18/21/42/no-entry-37967_960_720.png" /></td>
+    <td align="left" valign="middle"><H2 style="color:#FFFFFF; font-family:Arial, Helvetica, sans-serif; font-size:24px">We are sorry,<br />You are not Authorized<br />to see this content...</H2></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
